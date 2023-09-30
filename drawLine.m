b3f829f0c4039ef89fcb8627b7d0eafd13226b1b
MeganPratt <145616281+MeganPratt@users.noreply.github.com>
2023-09-30 12:01:00 -0700

    Update drawLine.m

--- drawLine.m
+++ drawLine.m
@@ -1,6 +1,5 @@
 function handle = drawLine (p, q, color)
-% This function draws a line between two 2D points p and q in homogeneous
-coordinates.
+% This function draws a line between two 2D points p and q in homogeneous coordinates.
 % extract x coord from points
 x = [p(1) ; q(1)];
 % extract y coord from points
