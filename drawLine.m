1bc14b39ed63090462d689e38ab3d69d87e84025
MeganPratt <145616281+MeganPratt@users.noreply.github.com>
2023-09-30 11:59:49 -0700

    Update drawLine.m
    My original wasn't lined up with the format I had in the Readme file so I updated both.

--- drawLine.m
+++ drawLine.m
@@ -1,11 +1,11 @@
-function lineHandle = drawLine ( pointA, pointB, color)
-% This function takes two points pointA, pointB and draws a line between them
-% with color color
-
-%extract the x coordinates from the points
-x=[ pointA(1) ; pointB(1)];
-y=[ pointA(2); pointB(2)];
-
-lineHandle = plot(x, y, color);
-set(lineHandle,"LineWidth",3);
+function handle = drawLine (p, q, color)
+% This function draws a line between two 2D points p and q in homogeneous
+coordinates.
+% extract x coord from points
+x = [p(1) ; q(1)];
+% extract y coord from points
+y = [p(2) ; q(2)];
+% plot
+handle = plot(x,y,color);
+set(handle,"LineWidth",3);
 endfunction
