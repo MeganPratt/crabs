c7ee51dd368097aaa55cf7cbc7fdeca9d3eb8a19
MeganPratt <meganpratt0105@gmail.com>
2023-10-14 16:20:05 -0700

    Tried to add more detail to captain

--- getCapt.m
+++ getCapt.m
@@ -45,28 +45,41 @@ captPt6 = [ captSize; -captSize; 1]; %right shoulder
 
 %head
 captPt7 = [ captSize; -captSize/2; 1];     %right neck
-captPt8 = [ 2 *captSize; -captSize/2; 1]; %right top of head
+captPt8 = [ 2*captSize; -captSize/2; 1]; %right top of head
 captPt9 = [ 2*captSize; captSize/2; 1];  %left top of head
 captPt10 = [ captSize; captSize/2; 1];   %left neck
+captPt23 = [ captSize/2; 0; 1];    %beard bottom
+captPt24 = [ 3*captSize/2; -captSize/2; 1];   %beard top left
+captPt25 = [ 2*captSize; -captSize; 1];   %hat left
+captPt26 = [ 11*captSize/4; 0; 1];   %hat top
+captPt27 = [ 2*captSize; captSize; 1];  %hat right
+captPt28 = [ 3*captSize/2; captSize/2; 1];   %beard top right
 
 %hands
 captPt11 = [   0;   2*captSize; 1];  %left
 captPt12 = [   0;  -2*captSize; 1]; %right
 
-%right spear
-captPt13=[ 3*captSize; -2*captSize; 1];  %spear point
-captPt14=[    -captSize; -2*captSize; 1];  %spear end
-
-%left spear
-captPt15=[ 3*captSize; 2*captSize; 1]; %spear point
-captPt16=[ -captSize; 2*captSize; 1]; %spear end
+%net
+captPt13 = [ 0; -3*captSize; 1];
+captPt14 = [ -3*captSize/2; -2*captSize; 1];
+captPt18 = [ -captSize; -4*captSize; 1];
+captPt19 = [ -5*captSize/2; -3*captSize; 1];
+captPt20 = [ -5*captSize/2; -4*captSize; 1];
+captPt21 = [ -2*captSize; -5*captSize/2; 1];
+captPt22 = [ -captSize/2; -7*captSize/2; 1];
 
 %elbows
-captPt17=[ captSize/2; 3*captSize/2; 1]; %left
-captPt18=[ captSize/2; -3*captSize/2; 1]; %right
+captPt29 = [ -captSize/2; 3*captSize/2; 1];
+captPt17=[ captSize/2; -3*captSize/2; 1];
+
+#sword
+captPt16 = [ captSize/2; 3*captSize/2; 1];
+captPt15 = [ 3*captSize; 2*captSize; 1];
+captPt30 = [ captSize/2; 5*captSize/2; 1];
+captPt31 = [ -captSize/2; 2*captSize; 1];
 
 %captain matrix
-capt = [ captPt1, captPt2, captPt3, captPt4, captPt5, captPt6, captPt7, captPt8, captPt9, captPt10, captPt11, captPt12, captPt13, captPt14, captPt15, captPt16, captPt17, captPt18];
+capt = [ captPt1, captPt2, captPt3, captPt4, captPt5, captPt6, captPt7, captPt8, captPt9, captPt10, captPt11, captPt12, captPt13, captPt14, captPt15, captPt16, captPt17, captPt18, captPt19, captPt20, captPt21, captPt22, captPt23, captPt24, captPt25, captPt26, captPt27, captPt28, captPt29, captPt30, captPt31];
 
 endfunction
 
